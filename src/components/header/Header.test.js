cc907b401e2a0b3c585b472024a82a778f5f409f
AshleyLerma <lerma.scout@gmail.com>
2020-08-29 12:14:44 -0500

    create failing Header test

---
+++ src/components/header/Header.test.js
@@ -0,0 +1,18 @@
+// import dependencies
+import React from 'react';
+// import react-testing methods
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom/extend-expect'
+// import Header component
+import Header from './Header';
+
+
+test('loads and displays header', async () => {
+    // Arrange
+    const { container, asFragment, debug } = render(<Header />);
+    debug();
+    // Act - user is not interacting with this
+    // Assert
+    expect(container).toContainHTML(`<h1>Employee Directory</h1>`);
+  })
