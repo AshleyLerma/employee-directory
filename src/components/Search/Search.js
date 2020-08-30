77885522d9f239d2858b9c96ea3f314d34da6356
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 14:12:07 -0500

    added css to search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./Search.css";
-import { Form } from "react-bootstrap";
+import { Form, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Search extends Component {
   render() {
-    return <Form.Control type='text' placeholder='Search' />;
+    return (
+      <Row className='search'>
+        <Form.Control id='searchBar' type='text' placeholder='Search' />
+      </Row>
+    );
   }
 }
 
