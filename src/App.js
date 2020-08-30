b75cefa4dfba8b53dfb364181ab36d9b8ae3d310
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 13:40:36 -0500

    added search to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Search />
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,14 +4,14 @@ import "./Header.css";
 class Header extends Component {
   render() {
     return (
-      <div className='header'>
+      <header className='header'>
         <h2>Employee Directory</h2>
         <h6>
           Click on carrots to filter by heading or use the search box to narrow
           your results.
         </h6>
         <hr />
-      </div>
+      </header>
     );
   }
 }
