2e264b0db57fac590a99546facb47e6649494e0f
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 17:49:04 -0500

    failing table header test

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./Table.css";
-import "node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import "../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
+import { Row } from "react-bootstrap";
+var employees = [];
 
-class Table extends Component {
-  render() {
-    return;
-  }
-}
+class Table extends Component {}
 
 export default Table;
--- src/components/Table/Table.test.js
+++ src/components/Table/Table.test.js
@@ -4,16 +4,15 @@ import React from "react";
 import { render } from "@testing-library/react";
 // add custom jest matchers from jest-dom
 import "@testing-library/jest-dom/extend-expect";
-// import Header component
+// import Table component
 import Table from "./Table";
 
 describe("Table", () => {
   test("loads and displays table headers", async () => {
     // Arrange
-    const { container, debug } = render(<Table />);
-    debug();
+    const { container } = render(<Table />);
     // Act - user is not interacting with this
     // Assert
-    expect(container).toContainHTML();
+    expect(container).toContainHTML("Image", "Name", "Phone", "Email", "DOB");
   });
 });
