69ddafbd1811f01c150b13915a4bf44d26957fee
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 11:42:56 -0500

    update header to pass

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,11 +5,11 @@ class Header extends Component {
   render() {
     return (
       <div className='header'>
-        <h1>Employee Directory</h1>
-        <h3>
+        <h2>Employee Directory</h2>
+        <h6>
           Click on carrots to filter by heading or use the search box to narrow
           your results.
-        </h3>
+        </h6>
       </div>
     );
   }
