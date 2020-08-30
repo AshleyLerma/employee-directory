5221105a96cdb4d1398474e4451e7937babd9086
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 14:50:11 -0500

    clean up index.html

---
+++ src/components/Table/Table.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Search.css";
+import { Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Table extends Component {
+  render() {
+    return;
+  }
+}
+
+export default Table;
---
+++ src/components/Table/Table.test.js
@@ -0,0 +1,19 @@
+// import dependencies
+import React from "react";
+// import react-testing methods
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+// add custom jest matchers from jest-dom
+import "@testing-library/jest-dom/extend-expect";
+// import Header component
+import Table from "./Table";
+
+describe("Table", () => {
+  test("loads and displays table", async () => {
+    // Arrange
+    const { container, asFragment, debug } = render(<Table />);
+    debug();
+    // Act - user is not interacting with this
+    // Assert
+    expect(container).toContainHTML();
+  });
+});
