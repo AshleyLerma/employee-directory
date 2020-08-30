0036742b98fba572dfe3bce7f37debf850347e13
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 11:40:26 -0500

    updated header test

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -15,7 +15,7 @@ describe("Header", () => {
     // Act - user is not interacting with this
     // Assert
     expect(container).toContainHTML(
-      `<h1>Employee Directory</h1><h3>Click on carrots to filter by heading or use the search box to narrow your results.</h3>`
+      `<h2>Employee Directory</h2><h6>Click on carrots to filter by heading or use the search box to narrow your results.</h6>`
     );
   });
 });
