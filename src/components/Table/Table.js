1253eef1d520f20794452477a99a18483356b05c
AshleyLerma <lerma.scout@gmail.com>
2020-08-30 17:51:38 -0500

    passing table header test

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,6 +5,70 @@ import "../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-a
 import { Row } from "react-bootstrap";
 var employees = [];
 
-class Table extends Component {}
+class Table extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sortName: [],
+      sortOrder: [],
+    };
+    this.onSortChange = this.onSortChange.bind(this);
+    this.cleanSort = this.cleanSort.bind(this);
+  }
+
+  onSortChange(name, order) {
+    const sortName = [];
+    const sortOrder = [];
+
+    for (let i = 0; i < this.state.sortName.length; i++) {
+      if (this.state.sortName[i] !== name) {
+        sortName.push(this.state.sortName[i]);
+        sortOrder.push(this.state.sortOrder[i]);
+      }
+    }
+
+    sortName.push(name);
+    sortOrder.push(order);
+    this.setState({
+      sortName,
+      sortOrder,
+    });
+  }
+
+  cleanSort() {
+    this.setState({
+      sortName: [],
+      sortOrder: [],
+    });
+  }
+
+  render() {
+    const options = {
+      sortName: this.state.sortName,
+      sortOrder: this.state.sortOrder,
+      onSortChange: this.onSortChange,
+    };
+    return (
+      <Row>
+        <BootstrapTable data={employees} options={options} multiColumnSort={4}>
+          <TableHeaderColumn dataField='image'>Image</TableHeaderColumn>
+          <TableHeaderColumn dataField='name' isKey={true} dataSort={true}>
+            Name
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='phone' dataSort={true}>
+            Phone
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='email' dataSort={true}>
+            Email
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='dob' dataSort={true}>
+            DOB
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </Row>
+    );
+  }
+}
 
 export default Table;
